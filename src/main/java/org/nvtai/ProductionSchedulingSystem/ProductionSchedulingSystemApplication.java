c79f131b88c30ef336182bc6c9113894688683f4
Nvtai <buiblue123@gmail.com>
2024-10-17 08:24:58 +0700

    re-init project

---
+++ src/main/java/org/nvtai/ProductionSchedulingSystem/ProductionSchedulingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.nvtai.ProductionSchedulingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionSchedulingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionSchedulingSystemApplication.class, args);
+	}
+
+}
